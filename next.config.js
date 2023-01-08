95a82fff2365d97db9a1f133a4a21c9e71d3a7d9
Anyilo <anyilojoserangelarias@gmail.com>
2023-01-08 12:36:31 -0500

    Curso no finalizado

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains:['placeimg.com'],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
